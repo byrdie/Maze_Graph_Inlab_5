c30fa18622a4e58502cb36a169665e6757648a74
byrdie <byrdie@BATTLESTATION>
2014-03-07 09:44:21 -0700

    Made sure project runs.

--- Maze_Graph_Inlab_5/src/Maze_Graph/Maze.java
+++ Maze_Graph_Inlab_5/src/Maze_Graph/Maze.java
@@ -10,7 +10,6 @@ package Maze_Graph;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package mazesetupexample;
 
 import java.util.Random;
 import java.util.Scanner;
